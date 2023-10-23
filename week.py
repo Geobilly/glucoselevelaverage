cb170366b8a8e562983bd4c92ed401e240e76340
Geobilly <102395479+Geobilly@users.noreply.github.com>
2023-10-23 08:53:54 +0000

    Update week.py

--- week.py
+++ week.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -17,4 +18,5 @@ def calculate_weekly_average():
         return jsonify({'error': 'Invalid data format! Please provide a dictionary with 7 daily averages.'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
